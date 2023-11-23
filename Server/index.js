84710d1ea36f181002981297d4adef748ee19497
LGSDenzo173 <johnyb574@gmail.com>
2023-11-23 14:21:10 +0500

    3rd commit

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,37 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const Port = process.env.Port ||8000
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require("./models/Employee");
+const Port = process.env.Port || 8000;
 
-app.get("/", (req,res)=>{
-    res.send("hello World!")
-})
-// app.post("/home", (req,res)=>{
-//     res.send("Post Route")
-// })
+app.use(express.json());
+app.use(cors());
 
+mongoose.connect("mongodb://127.0.0.1:27017/employee");
 
-app.listen(Port,()=>{
-    console.log(`Server Listening on Port ${Port}`)   
-})
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  EmployeeModel.findOne({ email: email })
+  .then(user => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("the passwrod is Incorrect");
+      }
+    } else {
+      res.json("No record Found!");
+    }
+  });
+});
+app.post("/register", (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employees) => res.json(employees))
+    .catch((err) => res.json(err));
+});
+
+app.listen(Port, () => {
+  console.log(`Server Listening on Port ${Port}`);
+});
