ac4311843a5b1c4981ab5180755a33230d5d51f1
LGSDenzo173 <johnyb574@gmail.com>
2023-11-27 13:59:35 +0500

    3rd commit

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const EmployeeModel = require("./models/Employee");
 const Port = process.env.Port || 8000;
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 app.use(cors());
@@ -13,23 +14,32 @@ mongoose.connect("mongodb://127.0.0.1:27017/employee");
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  EmployeeModel.findOne({ email: email })
-  .then(user => {
+  EmployeeModel.findOne({ email: email }).then((user) => {
     if (user) {
-      if (user.password === password) {
-        res.json("Success");
-      } else {
-        res.json("the passwrod is Incorrect");
-      }
-    } else {
+      bcrypt.compare(password, user.password, (err,response)=>{
+        if(err){
+          res.json("the password is incorect")
+        }
+        if (response){
+          res.json("Success")
+        }
+      })
+   } else {
       res.json("No record Found!");
     }
   });
 });
+
 app.post("/register", (req, res) => {
-  EmployeeModel.create(req.body)
-    .then((employees) => res.json(employees))
-    .catch((err) => res.json(err));
+  const { name, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hashpassword) => {
+      EmployeeModel.create({ name: name, email: email, password: hashpassword })
+        .then((employees) => res.json(employees))
+        .catch((err) => res.json(err));
+    })
+    .catch((err) => console.log(err.message));
 });
 
 app.listen(Port, () => {
